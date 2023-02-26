d600e39ef33a0cab08f9493ea21e1f446560dbb5
Nazaruk-D <nazaruk-dima@mail.ru>
2023-02-26 20:09:02 +0300

    refactor: registration

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,21 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const uuid_1 = require("uuid");
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const cors = require('cors');
 const mysql = require('mysql');
 const app = (0, express_1.default)();
@@ -18,17 +30,17 @@ const endPoints = {
 const connection = mysql.createConnection({
     host: 'gateway01.eu-central-1.prod.aws.tidbcloud.com',
     port: 4000,
-    user: '4AfZkWpmjEUaTXa.root',
-    password: 'YKY0nwjX0Ho0R5xo',
-    database: 'letters',
+    user: '479ukXTghZsCFgw.root',
+    password: '2kcSGnAulyiZ0Jj2',
+    database: 'test',
     ssl: {
         minVersion: 'TLSv1.2',
         rejectUnauthorized: true
     }
 });
+const users = [];
 connection.connect((err) => {
     if (err) {
-        // return console.log('Ошибка подключения к БД')
         return console.log(JSON.stringify(err));
     }
     else {
@@ -54,12 +66,23 @@ app.get(endPoints.me, (req, res) => {
     res.status(200).json({ message: "response me request" });
     return console.log('Соединение закрыто');
 });
-app.post(endPoints.registration, (req, res) => {
-    let newUserData = req.body;
-    console.log(newUserData);
-    res.status(201).json(newUserData);
+app.post(endPoints.registration, (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { firstName, lastName, email, password, } = req.body;
+    // Check if user already exists
+    const userExists = users.find((user) => user.email === email);
+    if (userExists) {
+        return res.status(409).json({ message: 'User already exists' });
+    }
+    // Hash the password
+    const salt = yield bcrypt.genSalt(10);
+    const hashedPassword = yield bcrypt.hash(password, salt);
+    const userId = (0, uuid_1.v1)();
+    // Save the user to the database
+    const user = { userId, firstName, lastName, email, password: hashedPassword };
+    users.push(user);
+    res.status(201).json({ message: 'User registered successfully' });
     return console.log('Соединение закрыто');
-});
+}));
 app.post(endPoints.login, (req, res) => {
     let loginData = req.body;
     res.status(200).json(loginData);
@@ -67,4 +90,25 @@ app.post(endPoints.login, (req, res) => {
 });
 app.listen(PORT, () => {
     console.log(`I started listening port: ${PORT}`);
+});
+// Login with an existing user
+app.post('/login', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { email, password } = req.body;
+    // Check if user exists
+    const user = users.find((user) => user.email === email);
+    if (!user) {
+        return res.status(401).json({ message: 'Invalid credentials' });
+    }
+    // Check if password is correct
+    const passwordMatch = yield bcrypt.compare(password, user.password);
+    if (!passwordMatch) {
+        return res.status(401).json({ message: 'Invalid credentials' });
+    }
+    // Generate JWT token
+    const token = jwt.sign({ email }, 'secret');
+    res.status(200).json({ message: 'Logged in successfully', token });
+}));
+// Start the server
+app.listen(3000, () => {
+    console.log('Server started on port 3000');
 });
--- index.ts
+++ index.ts
@@ -1,10 +1,9 @@
 import express from 'express'
 import {v1} from "uuid";
-
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const cors = require('cors')
 const mysql = require('mysql')
-
-
 const app = express()
 const PORT = process.env.PORT || 7542;
 
@@ -19,19 +18,29 @@ const endPoints = {
 const connection = mysql.createConnection({
     host: 'gateway01.eu-central-1.prod.aws.tidbcloud.com',
     port: 4000,
-    user: '4AfZkWpmjEUaTXa.root',
-    password: 'YKY0nwjX0Ho0R5xo',
-    database: 'letters',
+    user: '479ukXTghZsCFgw.root',
+    password: '2kcSGnAulyiZ0Jj2',
+    database: 'test',
     ssl: {
         minVersion: 'TLSv1.2',
         rejectUnauthorized: true
     }
-})
+});
+
+
+// Mock user database
+type UsersType = {
+    userId: string
+    email: string
+    firstName: string
+    lastName: string
+    password: string
+}
+const users:UsersType[] = [];
 
 
 connection.connect((err: any) => {
     if (err) {
-        // return console.log('Ошибка подключения к БД')
         return console.log(JSON.stringify(err))
     } else {
         return console.log('Подключение успешно')
@@ -51,22 +60,36 @@ app.use(cors(corsOptions));
 const jsonBodyMiddleWare = express.json()
 app.use(jsonBodyMiddleWare)
 
+
 app.get(endPoints.startPage, (req, res) => {
     res.json({message: "hi from Express App"})
     return console.log('Соединение закрыто')
 })
+
 
 app.get(endPoints.me, (req, res) => {
     res.status(200).json({message: "response me request"})
     return console.log('Соединение закрыто')
 })
 
-app.post(endPoints.registration, (req, res) => {
-    let newUserData = req.body
-    console.log(newUserData)
-    res.status(201).json(newUserData)
+app.post(endPoints.registration, async (req, res) => {
+    const { firstName, lastName, email, password, } = req.body;
+    // Check if user already exists
+    const userExists = users.find((user) => user.email === email);
+    if (userExists) {
+        return res.status(409).json({ message: 'User already exists' });
+    }
+    // Hash the password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const userId = v1()
+    // Save the user to the database
+    const user = { userId, firstName, lastName, email, password: hashedPassword };
+    users.push(user);
+    res.status(201).json({ message: 'User registered successfully' });
     return console.log('Соединение закрыто')
 })
+
 
 app.post(endPoints.login, (req, res) => {
     let loginData = req.body
@@ -78,3 +101,42 @@ app.post(endPoints.login, (req, res) => {
 app.listen(PORT, () => {
     console.log(`I started listening port: ${PORT}`)
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Login with an existing user
+app.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+
+    // Check if user exists
+    const user = users.find((user) => user.email === email);
+    if (!user) {
+        return res.status(401).json({ message: 'Invalid credentials' });
+    }
+
+    // Check if password is correct
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (!passwordMatch) {
+        return res.status(401).json({ message: 'Invalid credentials' });
+    }
+
+    // Generate JWT token
+    const token = jwt.sign({ email }, 'secret');
+
+    res.status(200).json({ message: 'Logged in successfully', token });
+});
+
+// Start the server
+app.listen(3000, () => {
+    console.log('Server started on port 3000');
+});
