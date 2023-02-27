baba8a91879cdcf01fe23b80f832d37f31a4c35d
Nazaruk-D <nazaruk-dima@mail.ru>
2023-02-27 13:11:19 +0300

    test

--- authController.ts
+++ authController.ts
@@ -55,7 +55,7 @@ class authController {
             }
             const token = jwt.sign({email}, 'secret');
             // res.cookie('token', token)
-            res.cookie('token', token, { httpOnly: true, domain: process.env.NODE_ENV === 'development' ? '.localhost' : '.domain.com' })
+            res.cookie('token', token, { httpOnly: true, domain: process.env.NODE_ENV === 'development' ? '.localhost' : '.vercel.app' })
             res.status(200).json({message: 'Logged in successfully', token});
             return console.log('Соединение закрыто')
         } catch (e) {
