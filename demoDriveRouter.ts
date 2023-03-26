5f528daa47e9ff970f4b040bd3e52b04db88174d
naughtymor <znaknikita.it@gmail.com>
2023-03-26 21:36:03 +0300

    feat: create demo drive order

--- demoDriveRouter.ts
+++ demoDriveRouter.ts
@@ -6,9 +6,11 @@ const demoDriveController = require('./demoDriveController')
 const demoDriveEndPoints = {
     updateDemoDriveStatus: '/updateDemoDriveStatus',
     getDemoDriveOrders: '/getDemoDriveOrders',
+    createDemoDriveOrder: '/createDemoDriveOrder',
 }
 
 demoDriveRouter.put(demoDriveEndPoints.updateDemoDriveStatus, demoDriveController.updateDemoDriveStatus);
+demoDriveRouter.put(demoDriveEndPoints.createDemoDriveOrder, demoDriveController.createDemoDriveOrder);
 demoDriveRouter.get(demoDriveEndPoints.getDemoDriveOrders, demoDriveController.getDemoDriveOrders);
 
 module.exports = demoDriveRouter;
--- dist/demoDriveRouter.js
+++ dist/demoDriveRouter.js
@@ -5,7 +5,9 @@ const demoDriveController = require('./demoDriveController');
 const demoDriveEndPoints = {
     updateDemoDriveStatus: '/updateDemoDriveStatus',
     getDemoDriveOrders: '/getDemoDriveOrders',
+    createDemoDriveOrder: '/createDemoDriveOrder',
 };
 demoDriveRouter.put(demoDriveEndPoints.updateDemoDriveStatus, demoDriveController.updateDemoDriveStatus);
+demoDriveRouter.put(demoDriveEndPoints.createDemoDriveOrder, demoDriveController.createDemoDriveOrder);
 demoDriveRouter.get(demoDriveEndPoints.getDemoDriveOrders, demoDriveController.getDemoDriveOrders);
 module.exports = demoDriveRouter;
